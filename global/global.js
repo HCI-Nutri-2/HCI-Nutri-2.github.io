433e241bb1e7c909cd9da001f899641d0642e10e
HCI-Nutri-2 <hcinutri@gmail.com>
2024-06-08 09:32:04 +0000

    Add modal menu

--- global/global.js
+++ global/global.js
@@ -6,51 +6,142 @@ function scrollToE (element, offset) {
     }
 }
 
-function navigateToPage(pageUrl) {
-    window.location.href = pageUrl;
-}
-
-function currPageName() {
-    return window.location.pathname.substring(path.lastIndexOf('/') + 1).replace('.html', '');
-}
+// header logic
 
 function loadHeader () {
-    const login = document.getElementById('login').addEventListener('click', function() {navigateToPage('../login/login.html');});;
-    const menu = document.getElementById('menu').addEventListener('click', function() {navigateToPage('../index.html');});;
-    const history = document.getElementById('history').addEventListener('click', function() {navigateToPage('../order-history/history.html');});;
-    const account = document.getElementById('account').addEventListener('click', function() {navigateToPage('../profile/profile.html');});;
+    const menu = `<div id="menu" onclick="navToPage('../index.html')"><img src="../global/icons/user.png"></div>`;
+    const history = `<div id="history" onclick="navToPage('../history/history.html')"><img src=""></div>`;
+    const account = `<div id="account" onclick="navToPage('../profile/profile.html')"><img src="../global/icons/user.png"></div>`;
+    const container = document.getElementById('header-button-wrapper')
+
+    const isLoggedIn = localStoreGet("isLoggedIn") === "true";
+    const currentPage = currPageName();
+    container.innerHTML = '';
+
+    if (isLoggedIn) {
+        if (currentPage === "history") {
+            container.innerHTML += menu + account;
+        } else if (currentPage === 'menu') {
+            container.innerHTML += history + account;
+        } else if (currentPage === 'account') {
+            container.innerHTML += menu + history;
+        } else {
+            container.innerHTML += menu + history + account;
+        }
+    } else {
+        container.innerHTML = `<button class="res-button button bcol1 col3 pad10 rad5 mont bold" onclick="toggleLogin(true)">Login / Sign Up</button>`;
+    }
 }
 
-// login form
-var modal = document.getElementById("login-form");
+// login modal
+    var modal = document.getElementById("login-form");
 
-function toggleLogin(state = false) {
-    if (state === true) {localStorage.setItem("isLoggedIn", "true")}
-    modal.style.display = (modal.style.display === 'none') ? 'flex' : 'none';
-    toggleStates('login')
+    function toggleLogin(state = false) {
+        if (state === true) {localStoreSet("isLoggedIn", "true");}
+        modal.style.display = (modal.style.display === 'none') ? 'flex' : 'none';
+        toggleStates('login');
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            toggleLogin();
+        }
+    }   
+
+    function toggleStates (show) {
+        document.getElementById('login').classList.add('none');
+        document.getElementById('reset').classList.add('none');
+        document.getElementById('regis').classList.add('none');
+
+        if (show === 'reset'){toggleResStates('reset-form1');}
+        document.getElementById(show).classList.remove('none');
+    }
+
+    function toggleResStates (show) {
+        document.getElementById('reset-form1').classList.add('none');
+        document.getElementById('reset-form2').classList.add('none');
+        document.getElementById('reset-form3').classList.add('none');
+
+        document.getElementById(show).classList.remove('none');
+    }
+
+// add-ons modal
+const addons = document.getElementById("addons-form");
+function toggleAddons(element) {
+    addons.style.display = (addons.style.display === 'none') ? 'flex' : 'none';
+    const img = addons.querySelector('img');
+    img.src = element.querySelector('img').src
+}
+function loadAddons () {
+    const fill = `
+    <div class="form-wrapper container rad10 bcol3">
+
+            <div class="vcontainer left-wrapper">
+                <div class="container elipsis">
+                    <p class="container moorley ts50 col1">Add-ons & Modifications</p>
+                </div>
+                <div class="container img-container w100 f1 bcol1 rad10 pos-rel">
+                    <img src="../global/icons/image-placeholder.png" class="fit-img">
+                    <div class="pos-abs container w100 h100" style="box-shadow: inset 0 -80px 50px -40px var(--3)"></div>
+                    <div class="item-name-overlay w100 container sbet pos-abs">
+                        <div class="vcontainer pad5 lineh15 elipsis">
+                            <p class="fonseca ts25 col2">Item Name</p>
+                            <p class="mont bold ts10 col1">Item Description</p>
+                        </div>
+                        <div class="container pad10 down">
+                            <input type="number" class="res-input bcol1 col3 pad5 rad10 right mont bold ts15" value="0" max="99" min="0">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="vcontainer right-wrapper">
+                <div class="container sbet down">
+                    <div class="container">
+                        <p class="button col2 hov-ul moorley ts50" onclick="">Details</p>
+                        <p class="button col2 hov-ul moorley ts50" onclick="">Info</p>
+                    </div>
+                    <div class="container gap5 col4">
+                        <p class="moorley ts25">$</p>
+                        <p class="noir ts25">20</p>
+                    </div>
+                </div>
+
+                <div class="vcontainer f1 gap25">
+                    <div class="ovy-a vcontainer f1 bcol4 rad10 pad10">
+                        <div id="addons-modal-content" class="vcontainer noir ts15 col3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia magnam qui perspiciatis dolor blanditiis quos voluptatem vel perferendis deleniti modi quisquam asperiores assumenda eos quo aperiam distinctio tempora adipisci. Libero, labore minus laudantium porro repellat velit corporis maiores dignissimos voluptates officiis magni, accusantium vel, nostrum aut perspiciatis sunt voluptatem! Fugiat eius aliquid asperiores, repellat cum officia velit iusto omnis pariatur, nobis, mollitia doloremque eum. Nulla atque accusantium sequi eligendi repellendus, saepe optio quo animi distinctio explicabo doloribus odit numquam corporis beatae. Inventore cum, libero sapiente, porro voluptates necessitatibus, aut nulla neque temporibus quod veniam? Saepe totam facere autem velit!?
+                        </div>
+                    </div>
+
+                    <div class="container sbet gap25 noir ts20">
+                        <div onclick="toggleAddons()" class="container button center jcenter bcol2 col3 rad5 noir"><p>Cancel</p></div>
+                        <div onclick="toggleAddons()" class="container button center jcenter bcol1 col3 rad5 noir f1 elipsis"><p>Save to Cart</p></div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    `
+
+    addons.innerHTML += fill;
+
+    window.onclick = function(event) {
+        if (event.target == addons) {
+            toggleAddons();
+        }
+    }
+
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
+// Menu click
+function menuClick (element) {
+    if (localStoreGet("isLoggedIn") === "true") {
+        toggleAddons(element);
+    }else {
         toggleLogin();
     }
-}   
-
-function toggleStates (show) {
-    document.getElementById('login').classList.add('none');
-    document.getElementById('reset').classList.add('none');
-    document.getElementById('regis').classList.add('none');
-
-    document.getElementById(show).classList.remove('none');
-    if (show === 'reset'){toggleResStates('reset-form1')}
-}
-
-function toggleResStates (show) {
-    document.getElementById('reset-form1').classList.add('none');
-    document.getElementById('reset-form2').classList.add('none');
-    document.getElementById('reset-form3').classList.add('none');
-
-    document.getElementById(show).classList.remove('none');
 }
 
 // Carousel
@@ -99,17 +190,40 @@ var CarouselModule = (function() {
     };
 })();
 
-if (typeof(Storage) !== "undefined") {
-    var isLoggedIn = localStorage.getItem("isLoggedIn");
-    if (!isLoggedIn) {
-        isLoggedIn = false;
+// utility functions
+function tryRun (fn) {
+    try {
+        fn();
+    } catch (error) {
+        console.error(`Error executing function ${fn.name}:`, error);
     }
-} else {
-    console.log("Sorry, your browser does not support web storage...");
+}
+
+function localStoreGet (key, def = false) {
+    if (typeof(Storage) !== "undefined") {
+        let val = localStorage.getItem(key);
+        if (val === null) {val = def;}
+        return val;
+    }
+    return def;
+}
+
+function localStoreSet (key, val) {
+    if (typeof(Storage) !== "undefined") {localStorage.setItem(key, val);}
+}
+
+function currPageName () {
+    return document.body.id;
+}
+
+function navToPage (location) {
+    window.location.href = location;
 }
 
 // Global DOMConLoaded
 document.addEventListener('DOMContentLoaded', function () {
-    CarouselModule.init();
-    loadHeader();
+    var isLoggedIn =  localStoreGet("isLoggedIn")
+    tryRun(CarouselModule.init);
+    tryRun(loadHeader);
+    tryRun(loadAddons);
 });
---
+++ history/history.js
@@ -0,0 +1,114 @@
+function genRInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function genRID(date) {
+    const year = date.getFullYear();
+    const randomPart = () => Math.random().toString(36).substring(2, 8).toUpperCase();
+    return `${year}-${randomPart()}-${randomPart()}`;
+}
+
+function genRPrice() {
+    return 'Rp. ' + (Math.random() * 1000000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.').replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, '$1.');
+}
+
+function genRDate(refDate) {
+    const newDate = new Date(refDate);
+    newDate.setDate(refDate.getDate() - Math.floor(Math.random() * 30) + 1);
+    return newDate;
+}
+
+function formatDate(date) {
+    const [day, month, year] = [date.getDate(), date.getMonth() + 1, date.getFullYear()];
+    return `${String(day).padStart(2, '0')}/${String(month).padStart(2, '0')}/${year}`;
+}
+
+function genItems(numContainers, containerId) {
+    const container = document.getElementById(containerId);
+    if (!container) return console.error("Container element not found!");
+
+    let currentDate = new Date();
+    let items = [];
+
+    for (let i = 0; i < numContainers; i++) {
+        const randomDate = currentDate = genRDate(currentDate);
+        const htmlContent = `
+            <div class="vcontainer gap15">
+                <div class="item container w100 sbet">
+                    <div class="container col3 bold">
+                        <p class="a">${genRID(randomDate)}</p>
+                        <p class="b">${formatDate(randomDate)}</p>
+                        <p class="c">Delivered</p>
+                        <p class="d">${genRPrice()}</p>
+                    </div>
+                    <div class="container gap15">
+                        <button class="res-button button bcol-tsp col3 pad5 mont bold ts15 hov-ul">Details</button>
+                        <button class="res-button button bcol3 col2 rad5 pad5 mont bold ts15">Order Again</button>
+                    </div>
+                </div>
+                <hr>
+            </div>
+        `;
+        items.push(htmlContent);
+    }
+
+    let index = 0;
+    const itemsPerBatch = 20;
+
+    function loadMoreItems() {
+        for (let i = 0; i < itemsPerBatch && index < items.length; i++, index++) {
+            container.innerHTML += items[index];
+        }
+        if (index >= items.length) {
+            observer.unobserve(loadingIndicator);
+            loadingIndicator.style.display = 'none';
+        }
+    }
+
+    const loadingIndicator = document.getElementById('loading');
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            loadMoreItems();
+        }
+    });
+
+    observer.observe(loadingIndicator);
+    loadMoreItems();
+}
+
+(function () {
+    var modal = document.getElementById("login-form");
+    
+    function toggleLogin(state = false) {
+        if (state === true) {localStorage.setItem("isLoggedIn", "true");}
+        modal.style.display = (modal.style.display === 'none') ? 'flex' : 'none';
+        toggleStates('login');
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            toggleLogin();
+        }
+    }   
+    
+    function toggleStates (show) {
+        document.getElementById('login').classList.add('none');
+        document.getElementById('reset').classList.add('none');
+        document.getElementById('regis').classList.add('none');
+    
+        if (show === 'reset'){toggleResStates('reset-form1');}
+        document.getElementById(show).classList.remove('none');
+    }
+    
+    function toggleResStates (show) {
+        document.getElementById('reset-form1').classList.add('none');
+        document.getElementById('reset-form2').classList.add('none');
+        document.getElementById('reset-form3').classList.add('none');
+    
+        document.getElementById(show).classList.remove('none');
+    }
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+    genItems(genRInt(30, 1000000), "container");
+});
